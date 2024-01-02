e4d6e6ccd02afee08545c1e4d17e17cf53999797
cmg8431 <cmg8431@gmail.com>
2024-01-02 23:34:45 +0900

    feat(core): add slack core messaging functionality

---
+++ src/core/index.ts
@@ -0,0 +1,81 @@
+export interface SlackParameters {
+  channel?: string;
+  username?: string;
+  text?: string;
+  icon_emoji?: string;
+}
+
+/**
+ * Performs the fetch request and formats the data for ogs
+ *
+ * @param {string} url - URL to fetch
+ * @param {object} options - options for ogs
+ * @returns {Promise<{ data: string, response: { status: number, headers: object } }>} formatted request data and response
+ */
+export class Slack {
+  private webhookURL: string;
+
+  /**
+   * Creates a new instance of the Slack class.
+   * @param {string} webhookURL - The URL of the Slack webhook.
+   * @throws {Error} Throws an error if webhookURL is not provided.
+   */
+  constructor(webhookURL: string) {
+    if (!webhookURL) {
+      throw new Error('Need a webhookURL!');
+    }
+    this.webhookURL = webhookURL;
+  }
+
+  /**
+   * Posts a message to a Slack channel.
+   * @param {SlackParameters} options - Options for configuring the Slack message.
+   * @returns {Promise<Response>} A Promise that resolves to the fetch response.
+   */
+  post({
+    text = '<hello, world>',
+    channel = '#general',
+    username = 'bot',
+    icon_emoji = ':iphone:',
+  }: SlackParameters = {}): Promise<Response> {
+    const payload: SlackParameters = {
+      text,
+      channel,
+      username,
+      icon_emoji,
+    };
+
+    const body = this.createRequestBody(payload);
+    return this.sendMessage(body);
+  }
+
+  /**
+   * Creates the request body for the Slack message.
+   * @param {SlackParameters} payload - The payload for the Slack message.
+   * @returns {string} The formatted request body.
+   */
+  private createRequestBody(payload: SlackParameters): string {
+    return new URLSearchParams({
+      payload: JSON.stringify(payload),
+    }).toString();
+  }
+
+  /**
+   * Sends the Slack message using fetch.
+   * @param {string} body - The request body for the Slack message.
+   * @returns {Promise<Response>} A Promise that resolves to the fetch response.
+   */
+  private sendMessage(body: string): Promise<Response> {
+    return fetch(this.webhookURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body,
+    });
+  }
+}
+
+export const test = () => {
+  console.log('test');
+};
